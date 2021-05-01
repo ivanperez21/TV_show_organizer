65f4aa7e136dcbfa8f2e8ce3992bc0883b8ce52b
ivanperez21 <ivanperez21@protonmail.com>
2021-04-30 23:57:21 -0700

    format prep video file string

--- re_org.py
+++ re_org.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
 import os
 
+#list with all file extention
+VIDEO_EXTENSIONS = ('.wmv', '.avi', '.mkv', '.mp4', '.mpg', '.mpeg', '.mov')
 
 #input for working dir
-root_path = input("Provide path to files:\n")
+root_path = input('Provide path to files:\n')
 #allow linux relative file path i.e. ~/
 os.chdir(os.path.expanduser(root_path))
 print(root_path)
 #reset path variable to relative path
 root_path = os.getcwd()
 
-exten = '.mkv'
 
-#function of video file extensions
-#def vid_files(vid_dir, vid_exten=['avi', 'mp4', 'mkv', 'mov']):
-#    ''' Print files in vid_dir with extensions in vid_exten, recursively.'''
-
-#check if file or Directory
-#if os.path.isfile()
-#walking directory
-print("******Start os walk**********")
-for dirpath, subdir, files in os.walk(root_path):
-    for name in files:
-        if name.endswith(exten):
-            print(name)
-    #print("files:", files)
-    print('*' * 25)
-print("******End os walk**********")
+#loop through and walk every dir in path and print out video Files
+for root, subdir, files in os.walk(root_path):
+    for video_filename in files:
+        if video_filename.endswith(VIDEO_EXTENSIONS):
+            print(video_filename)
+print('*' * 80)
+#TODO: loop through each file and format the string to remove file extension and split on '.'
+for root, subdir, files in os.walk(root_path):
+    for video_filename in files:
+        if video_filename.endswith(VIDEO_EXTENSIONS):
+            for exten in VIDEO_EXTENSIONS:
+                video_filename = video_filename.strip(exten)
+            print(video_filename.split('.'))
